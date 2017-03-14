9d835a0e88d8e5c370fefe23e7c05275c6fc78a2
Matthew Di Meglio <mdimeglio@optusnet.com.au>
2017-03-14 19:06:28 +1100

    Added decompose.c (q8.c) to the lab

--- week03/q8.c
+++ week03/q8.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-	
-
-	return 0;
+    int number = 1023;
+    
+    while(number > 0) {
+        int lastDigit = number %10;
+        printf("%d \n", lastDigit);
+        number = number / 10;
+    }
+    return 0;
 }
